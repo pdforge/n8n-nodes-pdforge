599ce3d65371fb8b6fcba880da8c68bef8801d5e
mabreum <marcelo.abreu.miranda@gmail.com>
2025-01-27 12:09:40 -0300

    credentials

---
+++ credentials/PdforgeApi.credentials.ts
@@ -0,0 +1,25 @@
+import { IAuthenticateGeneric, ICredentialType, INodeProperties } from 'n8n-workflow';
+
+export class PdforgeApi implements ICredentialType {
+	name = 'PdforgeApi';
+	displayName = 'Pdforge API';
+	documentationUrl = 'https://docs.pdforge.com/getting-started/authentication';
+
+	properties: INodeProperties[] = [
+		{
+			displayName: 'API Key',
+			name: 'apiKey',
+			type: 'string',
+			default: '',
+		},
+	];
+
+	authenticate: IAuthenticateGeneric = {
+		type: 'generic',
+		properties: {
+			headers: {
+				Authorization: `Bearer {{$credentials.apiKey}}`,
+			},
+		},
+	};
+}
