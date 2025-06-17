a8367d949b983b45a2c2dec1cd95d0d55dcbdade
mabreum <marcelo.abreu.miranda@gmail.com>
2025-06-17 11:58:36 -0300

    changing name

--- nodes/Pdforge/descriptions/TemplateFields.ts
+++ nodes/Pdforge/descriptions/TemplateFields.ts
@@ -16,7 +16,7 @@ export const templateFields: INodeProperties[] = [
 		required: true,
 		default: '',
 		description:
-			'The template ID you want to use. Choose from the list, or specify an ID. Choose from the list, or specify an ID using an <a href="https://docs.n8n.io/code-examples/expressions/">expression</a>.',
+			'Choose from the list, or specify an ID using an <a href="https://docs.n8n.io/code-examples/expressions/">expression</a>',
 	},
 	{
 		displayName: 'Variables',
